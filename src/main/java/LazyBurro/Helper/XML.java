abcb5227763a1e3c6809493ab86cceddac5e36f3
Benjamin Harrington <ben@harringtonweb.com>
2018-01-04 14:51:03 -0600

    switch out jtidy for jsoup

--- src/main/java/LazyBurro/Helper/XML.java
+++ src/main/java/LazyBurro/Helper/XML.java
@@ -1,28 +1,9 @@
 package LazyBurro.Helper;
 
-import org.w3c.dom.Document;
-import org.w3c.tidy.Tidy;
-
-import java.io.ByteArrayInputStream;
-import java.io.ByteArrayOutputStream;
-import java.io.OutputStream;
+import org.jsoup.Jsoup;
 
 public class XML {
     public static String prettyPrint(String xml, int indent) {
-        Tidy tidy = new Tidy();
-        tidy.setXHTML(true);
-        tidy.setIndentContent(true);
-        tidy.setPrintBodyOnly(true);
-        tidy.setTidyMark(false);
-
-        // HTML to DOM
-        Document htmlDOM = tidy.parseDOM(new ByteArrayInputStream(xml.getBytes()), null);
-
-        // Pretty Print
-        OutputStream out = new ByteArrayOutputStream();
-        tidy.pprint(htmlDOM, out);
-
-        return out.toString();
-        //return xml;
+        return Jsoup.parse(xml).toString();
     }
 }
